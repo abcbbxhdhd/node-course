5623a3072bdc5ba30722f9c4feaab414e1602789
abcbbxhdhd <ajax76@protonmail.com>
2022-01-12 22:28:37 +0200

    Added sorting, pagination, filtering and updated few scripts

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-
+const Task = require('./task')
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -40,12 +40,36 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Invalid password')
             }
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
+}, {
+    timestamps: true
 })
 
+userSchema.virtual('tasks', {
+    ref: 'Task',
+    localField: '_id',
+    foreignField: 'owner'
+})
+
+userSchema.methods.toJSON = function() {
+    const user = this
+    const userObject = user.toObject()
+
+    delete userObject.password
+    delete userObject.tokens
+    return userObject
+}
 userSchema.methods.generateAuthToken = async function () {
     const user = this
     const token = jwt.sign({_id: user._id.toString() }, 'thisisnodecourse')
+    user.tokens = user.tokens.concat({token})
+    await user.save()
     return token
 
 }
@@ -75,6 +99,13 @@ userSchema.pre('save', async function (next) {
     }
     console.log('just before saving')
 
+    next()
+})
+
+// Delete user tasks when user is removed
+userSchema.pre('remove', async function (next) {
+    const user = this
+    await Task.deleteMany({owner: user._id})
     next()
 })
 const User = mongoose.model('User', userSchema)
